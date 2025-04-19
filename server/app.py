fd118879b7d1933a4f7d36f780bbf2d536e4cc2d
Jeffrey <j.lo128456@gmail.com>
2025-04-19 12:45:04 +1000

    completed lab

--- server/app.py
+++ server/app.py
@@ -19,15 +19,55 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter(Animal.id == id).first()
+
+    if not animal:
+        return make_response('<h1>404 animal not found</h1>', 404)
+
+    response_body = f'''
+        <ul>Name: {animal.name}</ul>
+        <ul>Species: {animal.species}</ul>
+        <ul>Zookeeper: {animal.zookeeper.name}</ul>
+        <ul>Enclosure: {animal.enclosure.environment}</ul>
+    '''
+
+    return make_response(response_body, 200)
+
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+
+    if not zookeeper:
+        return make_response('<h1>404 zookeeper not found</h1>', 404)
+
+    response_body = f'''
+        <ul>Name: {zookeeper.name}</ul>
+        <ul>Birthday: {zookeeper.birthday}</ul>
+    '''
+
+    for animal in zookeeper.animals:
+        response_body += f'<ul>Animal: {animal.name} ({animal.species})</ul>'
+
+    return make_response(response_body, 200)
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+
+    if not enclosure:
+        return make_response('<h1>404 enclosure not found</h1>', 404)
+
+    response_body = f'''
+        <ul>Environment: {enclosure.environment}</ul>
+        <ul>Open to Visitors: {enclosure.open_to_visitors}</ul>
+    '''
+
+    for animal in enclosure.animals:
+        response_body += f'<ul>Animal: {animal.name} ({animal.species})</ul>'
+
+    return make_response(response_body, 200)
+
 
 
 if __name__ == '__main__':
